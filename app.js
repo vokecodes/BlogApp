26307f031c36868f07d35c4440c395ab2768b2c8
voke junior <vefe44@gmail.com>
2019-06-14 01:29:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express"),
+  app = express(),
+  port = 3000,
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
+
+// App Configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// Mongoose Setup, Schema & Model
+mongoose.connect("mongodb://localhost:27017/blog_app", {
+  useNewUrlParser: true
+});
+
+let blogScehma = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+});
+let Blog = mongoose.model("Blog", blogScehma);
+
+// RESTFUL Routes
+
+// Index Route
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+app.get("/blogs", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) console.log(err);
+    else res.render("index", { blogs: blogs });
+  });
+});
+// New Route
+app.get("/blogs/new", (req, res) => {
+  res.render("new");
+});
+
+// Create Route
+app.post("/blogs", (req, res) => {
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) console.log(err);
+    else res.redirect("/blogs");
+  });
+});
+
+// Show Route
+app.get("/blogs/:id", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) res.redirect("/blogs");
+    else res.render("show", { blog: foundBlog });
+  });
+});
+
+app.listen(port, () => console.log(`Blog App is listening at port ${port}`));
